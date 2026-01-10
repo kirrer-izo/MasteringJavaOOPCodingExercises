399ce4a75dec94045d2e15364a528a3d2679e3c7
Isaack Muchoki <isaackmuchoki55@gmail.com>
2026-01-10 10:36:49 +0300

    created class named Wall with 2 fields namely; heigh and width of type double. Generated two constuctors. First constuctor is no-args constructor and second ahs parameters width and height. Generated setter and getter methods for all fields and getArea method of type double that returns area.

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,15 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Wall wall = new Wall(5,4);
+        System.out.println("area = " + wall.getArea());
+
+        System.out.println("width = " + wall.getWidth());
+
+        wall.setHeight(-1.5);
+        System.out.println("height = " + wall.getHeight());
+
+        System.out.println("area = " + wall.getArea());
+
     }
 }
