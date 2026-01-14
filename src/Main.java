31f03fcf4eb1685c6ff88ecb7c07325bb81c72d8
Isaack Muchoki <isaackmuchoki55@gmail.com>
2026-01-14 09:36:58 +0300

    created classes namely Floor, Carpet and Calculator. The Floor class has two instance fields with name width and lenght of type double, the class has one constructor that sets the width and length value, finally, it has a method named getArea without any parameters and returns the calculated area. The Carpet class has one instance field with name cost of type double, the class has one constructor with parameter cost of type double, finally, it has a method named getCost without any parameters, and returns the value of cost field. The calculator class has two instance fields namely floor of type Floor, and carpet of type Carpet, it has one constructor with parameters floor of type Floor and carpet of type Carpet and it initializes the fields, finally, it has method named getTotalCost wothout any parameters, it returns the calculated total cost to cover the floor with the carpet.

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,14 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Wall wall = new Wall(5,4);
-        System.out.println("area = " + wall.getArea());
-
-        System.out.println("width = " + wall.getWidth());
-
-        wall.setHeight(-1.5);
-        System.out.println("height = " + wall.getHeight());
-
-        System.out.println("area = " + wall.getArea());
+        Carpet carpet = new Carpet(3.5);
+        Floor floor = new Floor(2.75, 4.0);
+        Calculator calculator = new Calculator(floor, carpet);
+        System.out.println("total= " + calculator.getTotalCost());
+        carpet = new Carpet(1.5);
+        floor = new Floor(5.4, 4.5);
+        calculator = new Calculator(floor, carpet);
+        System.out.println("total= " + calculator.getTotalCost());
 
     }
 }
